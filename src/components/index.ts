6cb182df079affe36899ef2c7a60615269f54944
Ibrahim <ibrahim@Ibrahims-MacBook-Air.local>
2024-10-31 22:08:06 +0600

    add vstack

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,5 @@ export * from './ui/layout/Box';
 export * from './ui/media-icons/FastImage';
 export * from './ui/media-icons/VectorIcon';
 export * from './ui/media-icons/IconButton';
+export * from './ui/layout/HStack';
+export * from './ui/layout/VStack';
