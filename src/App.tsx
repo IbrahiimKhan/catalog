bbe94297962b7bf80482878f28d5a830d2481ef4
Ibrahim <ibrahim@Ibrahims-MacBook-Air.local>
2024-11-01 13:07:57 +0600

    add reanimated

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,18 @@ import React, { type ReactElement } from 'react';
 import theme from '@/theme';
 
 import 'react-native-gesture-handler';
-import { Navigator } from './navigators';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
+import { Navigator } from './navigators';
 import store from './store/store';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export const App = (): ReactElement => {
   return (
     <Provider store={store}>
       <ThemeProvider theme={theme}>
-        <GestureHandlerRootView>
-          <SafeAreaProvider>
-            <Navigator />
-          </SafeAreaProvider>
-        </GestureHandlerRootView>
+        <SafeAreaProvider>
+          <Navigator />
+        </SafeAreaProvider>
       </ThemeProvider>
     </Provider>
   );
